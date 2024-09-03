aba9c462824842652fa56c6d976ec0908ec3b521
Remi Prebet <remi.prebet@lip6.fr>
2024-09-03 11:53:44 +0200

    more flexible plot functions

--- src/connectivity/plots.jl
+++ src/connectivity/plots.jl
@@ -1,9 +1,12 @@
 
 ## Plot functions
-function plot_graph(G, Vemph=[]; color="red", width=3, vert = true)
-    println("Plotting the graph")
+function plot_graph(G, Vemph::Vector{Vector{T}}=Vector{Vector{T}}(); color="red", width=3, vert=true, subplt=false) where T<:Any
+    if !subplt 
+        println("Plotting the graph")
+        plot(legend=false)
+    end
     V, E = G
-    plot(legend=false)
+    #col = distinguishable_colors(length(Vemph)+2)
     for e in E
         v1, v2 = [ map(Float64, V[ee]) for ee in e ]
         plot!([v1[1], v2[1]], [v1[2], v2[2]], lc=color, lw=width)
@@ -11,24 +14,29 @@ function plot_graph(G, Vemph=[]; color="red", width=3, vert = true)
     if vert
         scatter!( map(Float64, [v[1] for v in V]),  map(Float64, [v[2] for v in V]), mc="black", m=:diamond)
     end
-    scatter!( map(Float64, [V[i][1] for i in Vemph]),  map(Float64, [V[i][2] for i in Vemph]), mc=color, m=:diamond)
-    gui()
+    for i in 1:length(Vemph)
+        scatter!( map(Float64, [V[j][1] for j in Vemph[i]]),  map(Float64, [V[j][2] for j in Vemph[i]]), mc=color, m=:diamond)
+    end
+    #gui()
 end
 
-function plot_graphs(CG; width=3, vert=true)
-    println("Plotting the graph")
-    plot(legend=false)
+function plot_graph(G, Vemph::Vector{T} where T<:Any; color="red", width=3, vert=true, subplt=false)
+    plot_graph(G, [Vemph], color=color, width=width, vert=vert, subplt=subplt)
+end
+
+function plot_graph(G, Vemph::Dict{Int, Vector{T}} where T<:Any; color="red", width=3, vert=true, subplt=false)
+    plot_graph(G, Vemph|>values|>collect, color=color, width=width, vert=vert, subplt=subplt)
+end
+
+function plot_graphs(CG; width=3, vert=true, subplt=false)
+    if !subplt
+        println("Plotting the graph")
+        plot(legend=false)
+    end
     col = distinguishable_colors(length(CG)+2)
     for j in eachindex(CG)
-        (V, E), CVemph = length(CG[j][1])==2 ? CG[j] : (CG[j], [])
-        for e in E
-            v1, v2 = [ map(Float64, V[ee]) for ee in e ]
-            plot!([v1[1], v2[1]], [v1[2], v2[2]], lc=col[j+2], lw=width)
-        end
-        if vert
-            scatter!( map(Float64, [v[1] for v in V]),  map(Float64, [v[2] for v in V]), mc="black", m=:diamond)
-        end
-        scatter!( map(Float64, [V[i][1] for i in CVemph]),  map(Float64, [V[i][2] for i in CVemph]), mc=col[j+2], m=:diamond)
+        G, CVemph = length(CG[j][1])==2 ? CG[j] : (CG[j], Vector{Int}())
+        plot_graph(G, CVemph, color=col[j+2], subplt=true)
     end
     gui()
 end
@@ -36,18 +44,7 @@ end
 function plot_graph_comp(G, Vemph=[]; width=3, vert=true)
     println("Plotting the graph")
     plot(legend=false)
-    CG = connected_components(G, Vemph=Vemph)
-    col = distinguishable_colors(length(CG)+2)
-    for j in eachindex(CG)
-        (V, E), CVemph = length(Vemph)>1 ? CG[j] : (CG[j], [])
-        for e in E
-            v1, v2 = [ map(Float64, V[ee]) for ee in e ]
-            plot!([v1[1], v2[1]], [v1[2], v2[2]], lc=col[j+2], lw=width)
-        end
-        if vert
-            scatter!( map(Float64, [v[1] for v in V]),  map(Float64, [v[2] for v in V]), mc="black", m=:diamond)
-        end
-        scatter!( map(Float64, [V[i][1] for i in CVemph]),  map(Float64, [V[i][2] for i in CVemph]), mc=col[j+2], m=:diamond)
-    end
+    CG = connected_components(G, Vemph)
+    plot_graphs(CG, width=width, vert=vert, subplt=true)
     gui()
 end
