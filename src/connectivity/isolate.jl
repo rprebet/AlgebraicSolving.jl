4009bf087ec95dec74e1c1e8530ce6165ac7a2f0
Remi Prebet <remi.prebet@lip6.fr>
2024-09-17 20:32:51 +0200

    significant improvement of isolation + use default usolve

--- src/connectivity/isolate.jl
+++ src/connectivity/isolate.jl
@@ -1,21 +1,30 @@
 include("src/usolve/usolve.jl")
 
-function isolate(f; prec = 32, software="msolve")
+function isolate(f; prec = 32, software="usolve")
     # Univariate isolation using usolve
+    if total_degree(f) == 0
+        return []
+    end
     @assert is_univariate(f) "Not univariate polynomial"
 	if software == "usolve"
-		return getindex.(usolve(f, precision = prec, uspath="src/usolve/usolve"),2)
+		return usolve(f, precision = prec, uspath="AlgebraicSolving.jl/src/connectivity/src/usolve/usolve", output="inter")
     else
-        fvar = vars(f)[1]
-        sols = real_solutions(Ideal(vcat([f], [ t for t in gens(parent(f)) if t!=fvar ])), precision=prec, interval=true)
-        return getindex.(sols, var_index(fvar))
+        #println(f)
+        sols = real_solutions(Ideal([change_ringvar(f)]), precision=prec, interval=true)
+        return getindex.(sols, 1)
     end
 end
 
-function isolate_eval(f, ivar, val; prec=64)
+function isolate_eval(f, ivar, val; prec=64, software="usolve")
+    #print(val)
     # univariate isolation of roots of a bivariate polynomial f
     # whose ivar-th variable is evaluated at val
     # uses msolve
-    sols = real_solutions(Ideal([f, gens(parent(f))[ivar] - val]), precision=prec, interval=true)
-    return [ s[ivar%2+1] for s in sols ]
+    fev = change_ringvar(evaluate(f, [ivar], [val]))
+    CD = lcm(map(denominator, collect(coefficients(fev))))
+    fev *= CD
+
+    return isolate(fev, prec=prec, software=software)
+
+    
 end
