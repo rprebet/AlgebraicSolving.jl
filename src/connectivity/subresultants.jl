0e9f10d16733cab803b42fdd24626b3e6f02cc10
Remi Prebet <remi.prebet@lip6.fr>
2024-09-24 16:17:28 +0200

    subresultant interpolation + big changes in mmod_subresultant + fix fact_gcd + param_crit_split

--- src/connectivity/subresultants.jl
+++ src/connectivity/subresultants.jl
@@ -146,29 +146,33 @@ function interp_subresultants(P::MPolyRingElem{T}, Q::MPolyRingElem{T}, iv; list
 	# Modern computer Algebra Th 6.51.
 	degs = degrees(P)
 	n, d = degs[iv], degs[iu]
-	degsmax = [ (2*n-1-2*k)*d for k=0:d-2 ] 
-	Dmax = degsmax[end]>>1+1
+	degsmax = [ (2*n-1-2*k)*d for k=0:d-1 ] 
+	#Dmax = degsmax[end]>>1+1
 	##
-	xs = [ rand(A) for _=1:(degsmax[1]+1) ]
-	@time begin
-	Srev = [ subresultants(change_ringvar(evaluate(P,[iu],[xx]),[Vv]), change_ringvar(evaluate(Q,[iu],[xx]),[Vv])) for xx in xs ]
-	end
-	S,t = Nemo.polynomial_ring(A,Vu)
-	Sr = [ [ S(0) for _=1:i] for i=1:n-2 ]
-	for i = 1:n-2
-		for j = 1:i
-			ys = [ A(Srev[l][i][j]) for l=eachindex(Srev) ]
-			Sr[i][j] = interpolate(S, xs, ys)
-		end
+    B,t =polynomial_ring(A, Vv)
+    xs = A.(0:degsmax[1])
+    Srev = Vector{Vector{typeof(t)}}(undef, length(xs))
+    evPQ = [ [B(coefficients_of_univariate(evaluate(pq,[iu],[xx]))) for pq in [P,Q] ] for xx in xs]
+    Threads.@threads for i in 1:length(xs)
+	    Srev[i] = subresultants(evPQ[i][1], evPQ[i][2])
+    end
+    Srev = [ [ collect(coefficients(ss)) for ss in s] for s in Srev ]
+	C, = Nemo.polynomial_ring(A,Vu)
+	Sr = [ [ C(0) for _=1:i] for i=1:n ]
+    Lys = [ [ [ (try A(Srev[l][i][j]) catch; A(0) end) for l in eachindex(Srev) ] for j in 1:i ] for i in 1:n ]
+	for i = 1:n
+        # For the i-th subresultant
+        Threads.@threads for j in 1:i
+            # Each coefficient is interpolated
+            Sr[i][j] = interpolate(C, xs, Lys[i][j])
+        end
 	end
 	return Sr
 end
 
-function mmod_subresultants(P::QQMPolyRingElem, Q::QQMPolyRingElem, idx; list=false)
-    CD = [ lcm(map(denominator, collect(coefficients(f)))) for f in [P,Q] ]
-    PZ, QZ = change_coefficient_ring.(Ref(ZZ), [P,Q] .* CD)
-    #println(PZ,"\n",QZ)
-    sr = mmod_subresultants(PZ, QZ, idx; list=list)
+function mmod_subresultants(P::QQMPolyRingElem, Q::QQMPolyRingElem, idx; list=false, n_threads=Threads.nthreads(), n_ssr=-1,v=0)
+    PZ, QZ = change_coefficient_ring.(Ref(ZZ), int_coeffs.([P,Q]))
+    sr = mmod_subresultants(PZ, QZ, idx; list=list, n_threads=n_threads, n_ssr=n_ssr, v=v)
     return sr
     if list
         return [ change_coefficient_ring.(Ref(QQ),srp) for srp in sr ]
@@ -177,52 +181,135 @@ function mmod_subresultants(P::QQMPolyRingElem, Q::QQMPolyRingElem, idx; list=fa
     end
 end
 
-function mmod_subresultants(P::ZZMPolyRingElem, Q::ZZMPolyRingElem, idx; list=false)
-    prim = ZZ(2)<<(60)
+function mmod_subresultants(P::ZZMPolyRingElem, Q::ZZMPolyRingElem, idx; list=false, n_threads=Threads.nthreads(), n_ssr=0, v=0)
+    prim = ZZ(1)<<(61)
     L1, primprod = [], ZZ(1)
+    Ltemp = Vector{Vector{Vector{Vector{ZZRingElem}}}}(undef,n_threads+1)
     lcpq =  [ evaluate(leading_coefficient(l, 2),[0,0]) for l in [P,Q] ]
-    i=1
-    step=Threads.nthreads()
+    compt=n_threads
     while true
-        print("$i primes ")
+        v > 0 && print("$compt primes ")
         LP, LQ, Lprim = MPolyRingElem[], MPolyRingElem[], ZZRingElem[]
-        while length(Lprim) < step
+        while length(Lprim) < n_threads
             prim = next_prime(prim)
-            if !any(getindex.(divides.(Ref(prim), lcpq),1)) 
+            if !any(getindex.(divides.(lcpq, Ref(prim)),1)) 
                 Pprim, Qprim = [ change_coefficient_ring(residue_ring(ZZ, prim)[1], poly) for poly in [P,Q] ]
+                #Pprim, Qprim = [ change_coefficient_ring(GF(prim), poly) for poly in [P,Q] ]
                 push!.([LP,LQ,Lprim], [Pprim,Qprim,prim])
+            else
+                return prim, lcpq
             end
         end
-        Ltemp = Vector{Vector{ZZRingElem}}(undef,step)
-        Threads.@threads for j in 1:step
-            sr = subresultants(LP[j], LQ[j], idx)[1]
-            Ltemp[j] = lift.(Ref(ZZ), coefficients_of_univariate(sr))
+        Threads.@threads for j in 1:n_threads
+            sr = subresultants(LP[j], LQ[j], idx, list=true)
+            Ltemp[j] = [ [ lift.(Ref(ZZ), coefficients_of_univariate(srii)) for srii in sri ] for sri in sr ]
+            #sr = interp_subresultants(LP[j], LQ[j], idx)[1][1]
+            #Ltemp[j] = lift.(Ref(ZZ), coefficients(sr))
         end
-        if L1 != []
-            push!.([Ltemp, Lprim], [L1, primprod])
-        end
-        nLt = length(first(Ltemp))
-        @assert all(length.(Ltemp) .== nLt) "Specialization problem"
-        L2 = deepcopy(L1)
-        L1 = [ crt(getindex.(Ltemp,i), Lprim, true) for i in 1:nLt ]
 
-        L1 != L2 || break
+        if n_ssr > 0
+            for j in 1:n_threads
+                for k in eachindex(Ltemp[j])
+                    Ltemp[j][k] = Ltemp[j][k][end-min(k,n_ssr)+1:end]
+                end
+            end
+        end
+        
+        if L1 != []
+            Ltemp[n_threads+1] = copy(L1)
+            push!(Lprim, primprod)
+        end
+        #@assert allequal(length.(Ltemp[1:nthreads])) "Specialization problem"  
+        Nssr = n_ssr<0 ? length(last(first(Ltemp))) : n_ssr 
+        #println(Ltemp)
+        L2 = deepcopy(L1)
+        L1 = Vector{Vector{Vector{ZZRingElem}}}(undef, length(first(Ltemp)))
+        Threads.@threads for i in eachindex(L1)
+            L1[i] = Vector{Vector{ZZRingElem}}(undef, min(i, Nssr))
+            Threads.@threads for j in eachindex(L1[i])
+                #println("($i,$j)")
+                L1[i][j] = Vector{ZZRingElem}(undef, length(Ltemp[1][i][j]))
+                Threads.@threads for k in eachindex(L1[i][j])
+                    cfs = [ try Ltemp[p][i][j][k] catch; ZZ(0) end for p in eachindex(Lprim) ]
+                    L1[i][j][k] = crt(cfs, Lprim, true)
+                end
+            end
+        end
         #println(L1)
         primprod = prod(Lprim)
-        i+=step
-        println(ndigits(primprod, 10), " digits")
+        compt+=n_threads
+        v > 0 && println(ndigits(primprod, 10), " digits")
+        L1 != L2 || break
+        #compt < 20 || break
     end
-    return L1
+
+    uvar = parent(P).S[(idx+1)%2]
+    A, = polynomial_ring(ZZ, uvar)
+    sr = [ [ change_ringvar(A(ss),[uvar]) for ss in s ] for s in L1 ]
+    return sr
 end
 
-function fact_gcd(delta, Lsr, mult)
-    Lphi = [gcd(delta, Lsr[1])]
+function fact_gcd(delta::T, LP::Vector{T}) where (T <:PolyRingElem)
+    # delta : poly to factor w.r.t the polynomials in LP
+    Lphi = [gcd(delta, LP[1])]
     Ldelta = [delta/Lphi[1]]
     i = 2
-    while i<mult
-        push!(Lphi, gcd(Lphi[i-1], Lsr[i]))
-        push!(Ldelta, Ldelta[i-1]/Lphi[i])
+    while degree(Lphi[end])>0
+        push!(Lphi, gcd(Lphi[i-1], LP[i]))
+        push!(Ldelta, Lphi[i-1]/Lphi[i])
         i+=1
     end
-    return filter(s->degree(s[2])>0, (enumerate(Ldelta)))
+    return filter(s->degree(s[2])>0, Dict(enumerate(Ldelta)))
+end
+
+function fact_gcd(delta::T, LP::Vector{T}) where (T <:MPolyRingElem)
+    @assert is_univariate(delta) && all(is_univariate.(LP)) "Not univariate polynomial"
+    R = parent(delta)
+    A, = polynomial_ring(coefficient_ring(R), first(R.S))
+    out = fact_gcd(A(coefficients_of_univariate(delta)), A.(coefficients_of_univariate.(LP)))
+    return Dict([ (i, change_ringvar(f, [first(R.S)]) ) for (i,f) in out ])
+end
+
+function param_crit_split(f)
+    # Compute subresultants and factor the first subresultant
+    if total_degree(f) > 30
+        @time sr = mmod_subresultants(f, derivative(f, 2), 2, list=true, n_ssr=2)
+    else
+        @time sr = subresultants(f, derivative(f, 2), 2, list=true)
+    end
+    sqr = collect(factor_squarefree(sr[1][1]))
+
+    # Filter out factors with zero multiplicity and sort by multiplicity
+    sqr = sort!(filter(t -> t[2] > 0, sqr), by = t -> t[2])
+
+    # Group factors by multiplicity
+    sqrmult = unique(getindex.(sqr, 2))
+    # TODO: no product, manage each subfactor
+    group_sqr = Dict(m => prod([r[1] for r in sqr if r[2] == m]) for m in sqrmult)
+
+    # Initalization
+    singmult = filter(p->p*(p-1)<=sqrmult[end], 1:sqrmult[end])
+    param_crit = Dict(p => Vector{MPolyRingElem}[] for p in singmult)
+    lsr = [ s[end] for s in sr[2:singmult[end]] ]
+
+    # Critical points : multiplicity 1 in res
+    (1 in sqrmult) && push!(param_crit[1], [group_sqr[1], -sr[2][end-1], sr[2][end]])
+    # Nodes : multiplicity 2 in res
+    (2 in sqrmult) && push!(param_crit[2], [group_sqr[2], -sr[2][end-1], sr[2][end]])
+    # Other sing
+    filter!(m->!(m in [1,2]), sqrmult)
+    #TODO: simpler criterion for mult=p*(p-1)?
+    Ld = Vector{Dict{Int, MPolyRingElem}}(undef, length(sqrmult))
+    #Threads.@threads 
+    for k in eachindex(sqrmult)
+        Ld[k] = fact_gcd(group_sqr[sqrmult[k]], lsr)
+    end
+    for k in eachindex(Ld)
+        for (i, dji) in Ld[k]
+            push!(param_crit[i+1], [dji, -sr[i+1][end-1], i*sr[i+1][end]])
+        end
+    end
+
+    filter!(p->length(p[2])>0, param_crit)
+    return param_crit
 end
