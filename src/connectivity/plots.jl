68f6a4140b1fd15a22d456cdad411fe046cdf406
Remi Prebet <remi.prebet@lip6.fr>
2024-10-17 00:48:45 +0200

    temp savefig

--- src/connectivity/plots.jl
+++ src/connectivity/plots.jl
@@ -40,12 +40,43 @@ function plot_graphs(CG; width=3, vert=true, subplt=false)
         G, CVemph = length(CG[j][1])==2 ? CG[j] : (CG[j], Vector{Int}())
         plot_graph(G, CVemph, color=col[j+2], vert=vert, subplt=true)
     end
-    subplt || gui()
+    #subplt || gui()
+    savefig("/home/remi/Documents/gittravail/test.html")
+    replace_width_height_in_file("/home/remi/Documents/gittravail/test.html", 1000, 750)
 end
 
 function plot_graph_comp(G, Vemph=[]; width=3, vert=true, subplt=false)
     plot(legend=false)
     CG = connected_components(G, Vemph)
+    println(Vemph)
     plot_graphs(CG, width=width, vert=vert, subplt=true)
     subplt || gui()#savefig("/tmp/test.html")
+end
+
+function replace_width_height_in_file(filename, x, y)
+    # Open the file and read its content as a string
+    content = read(filename, String)
+
+    # Build the patterns to search for and the replacements
+    width_pattern = "\"width\": 600"
+    height_pattern = "\"height\": 400"
+
+    # Create the replacement strings with new values of x and y
+    new_width = "\"width\": $x"
+    new_height = "\"height\": $y"
+
+    # Replace the last occurrence of "width": 600
+    last_width_pos = findlast(occursin(width_pattern), split(content, "\n"))
+    if last_width_pos !== nothing
+        content = replace(content, width_pattern => new_width; count=1)
+    end
+
+    # Replace the last occurrence of "height": 400
+    last_height_pos = findlast(occursin(height_pattern), split(content, "\n"))
+    if last_height_pos !== nothing
+        content = replace(content, height_pattern => new_height; count=1)
+    end
+
+    # Write the modified content back to the file
+    write(filename, content)
 end
