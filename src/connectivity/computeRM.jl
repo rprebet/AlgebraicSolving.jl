cdd9fdfb5fdb73339462a5d731f4148630e9272c
Remi Prebet <remi.prebet@lip6.fr>
2025-04-10 10:16:46 +0200

    first attempt to add query points

--- src/connectivity/computeRM.jl
+++ src/connectivity/computeRM.jl
@@ -10,11 +10,11 @@ export change_ringvar, compute_minors, detmpoly, change_ringvar_mod, MPolyBuild
 
 include("Cannytools.jl")
 
-function computeRM(V::Ideal{T} where T <: QQMPolyRingElem, dimV::Int, Q=Vector{Vector{QQFieldElem}}([[]]) ; v=0)
+function computeRM(V::Ideal{T} where T <: QQMPolyRingElem, dimV::Int; Q=Vector{Vector{QQFieldElem}}([[]]), C=Vector{Vector{QQFieldElem}}([]), v=0)
 	# V is a variety of dimension dimV
 	# Q are base points with rational coefficients
 	# C are query points with rational coefficients
-
+  println(Q)
 	A = parent(V)
 	varias, nvarias = gens(A), nvars(A)
   hV = V.gens
@@ -41,32 +41,34 @@ function computeRM(V::Ideal{T} where T <: QQMPolyRingElem, dimV::Int, Q=Vector{V
         v>0 && println("Compute first the singular points")
         singFq = computepolarproj(0, Fq, dimV-e, newvarias, output="real", verb=max(v-1,0), nr_thrds=Threads.nthreads())
         @assert(length(singFq)==0, "Non-emtpy real sing locus!")
-        #@assert(degree(singFq.elim)==0, "Non-emtpy sing locus!")
 
         ## K(pi_1,Fq) ##
-        v >0 && println("First critical points")
+        v>0 && println("First critical points")
         K1Fq = computepolarproj(1, Fq, dimV-e, newvarias, output="interval", verb=max(v-1,0), nr_thrds=Threads.nthreads())
 
         ## K(pi_2, Fq) ##
-        v >0 && println("Second critical points")
+        v>0 && println("Second critical points")
         K2Fq = computepolarproj(2, Fq, dimV-e, newvarias, output="minors", verb=max(v-1,0), nr_thrds=Threads.nthreads())
         polar = change_ringvar(K2Fq.gens, A.S)
         push!(R, AlgebraicSolving.Ideal(vcat(polar, [fixvarias[j] - q[j] for j in 1:e])))
 
         ## Points with vertical tg in K(pi_2, Fq) ##
-        v >0 && println("Vertical tg points")
+        v>0 && println("Vertical tg points")
         K1WmFq = computepolarproj(2, K2Fq, 1, newvarias, output="interval", dimproj=0, verb=max(v-1,0), nr_thrds=Threads.nthreads())
 
         ## New base points ##
         K1W = vcat(K1Fq, K1WmFq)
         K1WRat = MidRationalPoints(getindex(K1W, 1))
-        newQ = [ vcat(q, [kv]) for kv in K1WRat ]
         # Heuristic to be proven
-        # newQ = newQ[2:end-1]
+        #K1WRat = K1WRat[2:end-1]
+        ##########
+        append!(K1WRat, [ c[e+1] for c in C if c[1:e]==q && !(c[e+1] in K1WRat) ]) |> sort!
+        newQ = [ vcat(q, [kv]) for kv in K1WRat ]
 
-        RFq = computeRM(V, dimV, newQ)
-
-        R = vcat(R, RFq)
+        if !isempty(newQ)
+          RFq = computeRM(V, dimV, Q=newQ, C=C)
+          R = vcat(R, RFq)
+        end
       end
   end
 
